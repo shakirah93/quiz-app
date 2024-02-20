fda1f755ffdd422a1b0ae32abf69394b11d41bca
oyinkan <skusman@student.lautech.edu.ng>
2024-02-20 19:52:28 +0000

    starter code for the quiz app

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const quiz = [
+  {
+    question: "What is the capital of France?",
+    options: ["Berlin", "Madrid", "Paris", "Rome"],
+    correctAnswer: "Paris"
+  },
+];
+
+
+function buildQuiz() {
+    // loop through the array of quiz
+    // as you loop each item of the array
+    // creating a heading element for your question
+    // loop through the "options", then create
+}
+
+function showResults() {
+    let score = 0;
+
+    
+}
